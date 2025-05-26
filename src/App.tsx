d0e6e1607861fa9e991c479d589bf21da73b9c9f
Alex Platz <me@oddx.xyz>
2025-05-26 13:34:12 -0500

    WIP 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
+import { ChatEvent, ChatLog, ChatMessage, toChatEvent, toChatLog, toChatMessage } from "#types";
+import randomName from "@scaleway/random-name";
 import "./index.css";
 
 import { ChangeEvent, useEffect, useState } from "react";
 // import useWebSocket, { ReadyState } from "react-use-websocket";
 // to fix weird default function error
 import { useWebSocket } from 'react-use-websocket/src/lib/use-websocket'
+import { match } from "mutch";
 
 const socketUrl = 'ws://127.0.0.1:4000'
+const username = randomName()
+type Typing = {
+  username: string,
+  lastInput: number
+}
 
 export const App = () => {
   const [text, setText] = useState('')
   const [history, setHistory] = useState<string[]>([])
-  const { lastMessage, sendMessage, sendJsonMessage } = useWebSocket(
+  const [typing, setTyping] = useState<Typing[]>([])
+  const { lastMessage, sendMessage } = useWebSocket(
     socketUrl,
     {
       onOpen: () => {
@@ -22,32 +31,64 @@ export const App = () => {
     }
   )
 
+
+
   useEffect(() => {
     lastMessage ?
-      // act on type, only add strings to history
-      // setHistory(prev => prev.concat(lastMessage.data)) :
-      parseMessage(lastMessage) :
+      handleMessage(JSON.parse(lastMessage.data)) :
       undefined
   }, [lastMessage])
 
+  // how to display and then timeout typing indicator?
+  useEffect(() => {
+    lastMessage ?
+      handleMessage(JSON.parse(lastMessage.data)) :
+      undefined
+  }, [typing])
+
+
+
   const handleSubmit = (e: ChangeEvent<HTMLFormElement>) => {
     e.preventDefault()
-    sendMessage(text)
-    // setHistory(prev => prev.concat(text))
+    sendMessage(toChatMessage(text, username))
     setText(_ => '')
   }
 
   // debounce this to 3 seconds
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     e.preventDefault()
-    sendJsonMessage({ lastInput: Date.now() })
+    sendMessage(toChatLog({ lastInput: Date.now() }, username))
     setText(e.target.value)
   }
 
-  // how to tell when regular string or json?
-  const parseMessage = ({ data }: MessageEvent<string>) => {
-    // console.log(typeof data, data)
-    console.log(JSON.parse(data))
+  const handleMessage = (data: ChatMessage | ChatEvent | ChatLog) => {
+    if (data.type === 'message' || data.type === 'event') {
+      setHistory(prev => prev.concat(data.content))
+    } else {
+      setTyping([
+        ...typing,
+        {
+          username: data.username,
+          lastInput: data.content.lastInput
+        }
+      ])
+    }
+  }
+
+
+  const typingIndicator = (usersTyping: Typing[]) => {
+    const users = usersTyping
+      .filter(({ lastInput }) =>
+        lastInput >= Date.now() - 5000
+      )
+
+    // rework mutch
+    return match(users.length, [
+      [, () => <p>Multiple people typing...</p>],
+      [=== 1, () => <p>{users.pop()?.username} is typing...</p>],
+    ],
+      () => null
+    )
   }
 
   // on typing send lastInput timestamp with username/id
@@ -56,6 +97,11 @@ export const App = () => {
     <div className="app">
       {history.map(item => <p>{item}</p>)}
       <form onSubmit={handleSubmit}>
+        {
+          // typing.lastInput >= Date.now() - 5000 ?
+          //   <p>{typing.username} is typing...</p> :
+          //   null
+        }
         <input
           onChange={handleChange}
           type="text"
--- src/server.ts
+++ src/server.ts
@@ -1,34 +1,39 @@
 import { serve } from "bun"
 import randomName from "@scaleway/random-name"
+import { toChatEvent } from "#types"
 
 const server = serve<{ username: string }, {}>({
   port: 4000,
   fetch: (req, server) =>
     !server.upgrade(req, {
-      data: {
-        username: randomName()
-      }
+      // data: {
+      //   username: randomName()
+      // }
     }) ?
       new Response("Upgrade failed", { status: 500 }) :
       undefined,
   websocket: {
     open: (ws) => {
       ws.subscribe('chat')
-      server.publish('chat', JSON.stringify({ message: 'new user has entered the chat' }))
+      server.publish(
+        'chat',
+        toChatEvent('new user has entered the chat')
+      )
     },
-    message: (ws, message) => {
-      // distinguish between message and event
-      // parse, don't validate?
-      // I think this needs to be a try/catch :/
-      server.publish('chat', JSON.stringify({
-        username: ws.data.username,
+    // check type
+    message: (_ws, message: string) => {
+      server.publish(
+        'chat',
         message
-      }))
-      console.log(typeof message, message)
+      )
+      console.log(message)
     },
     close: (ws) => {
       ws.unsubscribe('chat')
-      server.publish('chat', JSON.stringify({ message: 'user has left the chat' }))
+      server.publish(
+        'chat',
+        toChatEvent('user has left the chat')
+      )
     },
     perMessageDeflate: true,
   },
